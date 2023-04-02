339bef2c1a6277c26559ae8fbc7425b3aa566b11
Bilal Alac <bilalalac@gmail.com>
2023-04-02 19:31:53 +0200

    start JS

---
+++ script.js
@@ -0,0 +1,288 @@
+let beliebt = [
+    {
+        'meal': 'Cheeseburger',
+        'description': '',
+        'choice': '',
+        'price': 6.50,
+    },
+    {
+        'meal': 'Pizza Margherita',
+        'description': '',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 7.00,
+    },
+    {
+        'meal': 'Pizza Döner',
+        'description': 'mit Drehspießfleisch und Zwiebeln',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.50,
+    }
+]
+
+let salate = [
+    {
+        'meal': 'Gemischter Salat',
+        'description': '',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 8.50,
+    },
+    {
+        'meal': 'Krautsalat',
+        'description': 'mit Zwiebeln, Peperoni und Sauce, ohne gemischtem Salat',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 8.00,
+    },
+    {
+        'meal': 'Thunfisch Salat',
+        'description': 'mit Ei',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Chef Salat',
+        'description': 'mit Käse, Formvorderschinken, Thunfisch, Ei und Oliven',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 11.00,
+    },
+    {
+        'meal': 'Hawaii Salat',
+        'description': 'mit Formvorderschinken, Ananas und Käse',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Döner Salat',
+        'description': 'dazu Pizzabrot',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 11.50,
+    }
+]
+
+let pizza = [
+    {
+        'meal': 'Pizza Margherita',
+        'description': '',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 7.00,
+    },
+    {
+        'meal': 'Pizza Salami',
+        'description': 'mit Salami',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.00,
+    },
+    {
+        'meal': 'Pizza Schinken',
+        'description': 'mit Fomvorderschinken',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.00,
+    },
+    {
+        'meal': 'Pizza Pepperoniwurst',
+        'description': 'mit Pepperoniwurst',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.00,
+    },
+    {
+        'meal': 'Pizza Funghi',
+        'description': 'mit Champignons',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.50,
+    },
+    {
+        'meal': 'Pizza Tonno',
+        'description': 'mit Thunfisch und Zwiebeln',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.50,
+    },
+    {
+        'meal': 'Pizza Döner',
+        'description': 'mit Drehspießfleisch und Zwiebeln',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 8.50,
+    },
+    {
+        'meal': 'Pizza 4 Jahreszeiten',
+        'description': 'mit Formvorderschinken, Pilzen, Salami und Paprika',
+        'choice': 'Ø 28cm oder Ø 32cm',
+        'price': 9.00,
+    }
+]
+
+let calzone = [
+    {
+        'meal': 'Calzone Napoli',
+        'description': 'mit Formvorderschinken, Salami und Pilzen',
+        'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
+        'price': 9.00,
+    },
+    {
+        'meal': 'Calzone Tonno',
+        'description': 'mit Thunfisch',
+        'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Calzone Hawaii',
+        'description': 'mit Formvorderschinken und Ananas',
+        'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Calzone Frutti di Mare',
+        'description': 'mit Meeresfrüchten',
+        'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
+        'price': 11.00,
+    },
+    {
+        'meal': 'Calzone Döner',
+        'description': 'mit Dönerfleisch',
+        'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
+        'price': 9.50,
+    }
+]
+
+let drehspieß = [
+    {
+        'meal': 'Jumbo Döner',
+        'description': 'mit gemischtem Salat, Krautsalat und Zwiebeln',
+        'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
+        'price': 9.00,
+    },
+    {
+        'meal': 'Döner Fladenbrot',
+        'description': 'mit gemischtem Salat, Krautsalat und Zwiebeln',
+        'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
+        'price': 7.00,
+    },
+    {
+        'meal': 'Döner Hawaii',
+        'description': 'mit Ananas, Gouda und Tzatziki',
+        'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
+        'price': 8.00,
+    },
+    {
+        'meal': 'Vegetarischer Döner',
+        'description': 'mit Hirtenkäse und Salat',
+        'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
+        'price': 6.50,
+    },
+    {
+        'meal': 'Döner Premium',
+        'description': 'mit Drehspießfleisch und Sauce Hollandaise und mit Käse überbacken',
+        'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
+        'price': 8.50,
+    },
+    {
+        'meal': 'Döner Spezial',
+        'description': 'mit Drehspießfleisch, Pommes frites und Salat',
+        'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
+        'price': 8.50,
+    }
+]
+
+let teller = [
+    {
+        'meal': 'Dönerteller',
+        'description': 'mit Salat und Tzatziki',
+        'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
+        'price': 11.50,
+    },
+    {
+        'meal': 'Dönerteller Hollandaise',
+        'description': 'mit Sauce Hollandaise, Salat und Tzatziki und mit Käse überbacken',
+        'choice': '',
+        'price': 12.50,
+    },
+    {
+        'meal': 'Dönerauflauf',
+        'description': 'mit Nudeln und Sauce Hollandaise und mit Käse überbacken',
+        'choice': 'mit Pommes frites, mit Reis oder ohne Beilage.',
+        'price': 10.50,
+    },
+    {
+        'meal': 'Iskender',
+        'description': 'Drehspießfleisch auf gerösteten Brotwürfeln mit spezieller Tomatensauce, dazu Salat und Salatsauce',
+        'choice': 'mit Pommes frites, mit Reis oder ohne Beilage.',
+        'price': 12.00,
+    },
+    {
+        'meal': 'Chicken Picks (12 Stück)',
+        'description': 'mit Salat, ohne Drehspießfleisch',
+        'choice': 'mit Pommes frites, mit Reis oder ohne Beilage.',
+        'price': 10.50,
+    }
+]
+
+let pasta = [
+    {
+        'meal': 'Gnocchi mit Tomatensoße',
+        'description': '',
+        'choice': '',
+        'price': 8.50,
+    },
+    {
+        'meal': 'Gnocchi Chef',
+        'description': 'Kartoffelnudeln mit Sahnesauce, Champignons, Broccoli und mit Käse überbacken, ohne Brot',
+        'choice': '',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Gnocchi Gorgonzola',
+        'description': 'Kartoffelnudeln mit Sahnesauce, Champignons und Broccoli, ohne Brot',
+        'choice': '',
+        'price': 10.00,
+    },
+    {
+        'meal': 'Spaghetti Napoli',
+        'description': 'mit Tomatensoße',
+        'choice': '',
+        'price': 8.00,
+    },
+    {
+        'meal': 'Spaghetti Bolognese',
+        'description': 'mit Hackfleischsoße',
+        'choice': '',
+        'price': 9.00,
+    },
+    {
+        'meal': 'Spaghetti Carbonara',
+        'description': 'mit Formvorderschinken und Ei-Sahnesauce',
+        'choice': '',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Spaghetti Frutti di Mare',
+        'description': 'mit Meeresfrüchten',
+        'choice': '',
+        'price': 11.00,
+    }
+]
+
+let baguettes = [
+    {
+        'meal': 'Baguette Salami',
+        'description': 'mit Tomaten und Zwiebeln',
+        'choice': '',
+        'price': 9.00,
+    },
+    {
+        'meal': 'Baguette Schinken',
+        'description': 'mit Tomaten, Formvorderschinken und Zwiebeln',
+        'choice': '',
+        'price': 9.00,
+    },
+    {
+        'meal': 'Baguette Thunfisch',
+        'description': 'mit Tomaten und Zwiebeln',
+        'choice': '',
+        'price': 9.50,
+    },
+    {
+        'meal': 'Baguette Hawaii',
+        'description': 'mit Tomaten, Formvorderschinken und Ananas',
+        'choice': '',
+        'price': 9.50,
+    },
+]
+
