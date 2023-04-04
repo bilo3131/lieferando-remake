3d72dd489cf8f2abf756fa5bcf7824a6ba90bd57
Bilal Alac <bilalalac@gmail.com>
2023-04-04 02:02:58 +0200

    edit images

--- script.js
+++ script.js
@@ -1,315 +1,351 @@
 let beliebt = [
     {
+        'image': '',
+        'title': 'Beliebt'
+    },
+    {
         'meal': 'Cheeseburger',
         'description': '',
         'choice': '',
-        'price': 6.50,
+        'price': 6.50
     },
     {
         'meal': 'Pizza Margherita',
         'description': '',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 7.00,
+        'price': 7.00
     },
     {
         'meal': 'Pizza Döner',
         'description': 'mit Drehspießfleisch und Zwiebeln',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.50,
+        'price': 8.50
     }
 ]
 
 let salate = [
     {
+        'image': 'img/salad.jpg',
+        'title': 'Salate'
+    },
+    {
         'meal': 'Gemischter Salat',
         'description': '',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Krautsalat',
         'description': 'mit Zwiebeln, Peperoni und Sauce, ohne gemischtem Salat',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 8.00,
+        'price': 8.00
     },
     {
         'meal': 'Thunfisch Salat',
         'description': 'mit Ei',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Chef Salat',
         'description': 'mit Käse, Formvorderschinken, Thunfisch, Ei und Oliven',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 11.00,
+        'price': 11.00
     },
     {
         'meal': 'Hawaii Salat',
         'description': 'mit Formvorderschinken, Ananas und Käse',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Döner Salat',
         'description': 'dazu Pizzabrot',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 11.50,
+        'price': 11.50
     }
 ]
 
 let pizza = [
     {
+        'image': 'img/pizza.jpg',
+        'title': 'Pizza'
+    },
+    {
         'meal': 'Pizza Margherita',
         'description': '',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 7.00,
+        'price': 7.00
     },
     {
         'meal': 'Pizza Salami',
         'description': 'mit Salami',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.00,
+        'price': 8.00
     },
     {
         'meal': 'Pizza Schinken',
         'description': 'mit Fomvorderschinken',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.00,
+        'price': 8.00
     },
     {
         'meal': 'Pizza Pepperoniwurst',
         'description': 'mit Pepperoniwurst',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.00,
+        'price': 8.00
     },
     {
         'meal': 'Pizza Funghi',
         'description': 'mit Champignons',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Pizza Tonno',
         'description': 'mit Thunfisch und Zwiebeln',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Pizza Döner',
         'description': 'mit Drehspießfleisch und Zwiebeln',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Pizza 4 Jahreszeiten',
         'description': 'mit Formvorderschinken, Pilzen, Salami und Paprika',
         'choice': 'Ø 28cm oder Ø 32cm',
-        'price': 9.00,
+        'price': 9.00
     }
 ]
 
 let calzone = [
     {
+        'image': 'img/calzone.jpg',
+        'title': 'Calzone'
+    },
+    {
         'meal': 'Calzone Napoli',
         'description': 'mit Formvorderschinken, Salami und Pilzen',
         'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
-        'price': 9.00,
+        'price': 9.00
     },
     {
         'meal': 'Calzone Tonno',
         'description': 'mit Thunfisch',
         'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Calzone Hawaii',
         'description': 'mit Formvorderschinken und Ananas',
         'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Calzone Frutti di Mare',
         'description': 'mit Meeresfrüchten',
         'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
-        'price': 11.00,
+        'price': 11.00
     },
     {
         'meal': 'Calzone Döner',
         'description': 'mit Dönerfleisch',
         'choice': 'mit Ananas, mit Broccoli, mit Champignons, mit Drehspießfleisch, mit Ei und mehr.',
-        'price': 9.50,
+        'price': 9.50
     }
 ]
 
 let drehspieß = [
     {
+        'image': 'img/drehspieß.jpg',
+        'title': 'Drehspießgerichte'
+    },
+    {
         'meal': 'Jumbo Döner',
         'description': 'mit gemischtem Salat, Krautsalat und Zwiebeln',
         'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
-        'price': 9.00,
+        'price': 9.00
     },
     {
         'meal': 'Döner Fladenbrot',
         'description': 'mit gemischtem Salat, Krautsalat und Zwiebeln',
         'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
-        'price': 7.00,
+        'price': 7.00
     },
     {
         'meal': 'Döner Hawaii',
         'description': 'mit Ananas, Gouda und Tzatziki',
         'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
-        'price': 8.00,
+        'price': 8.00
     },
     {
         'meal': 'Vegetarischer Döner',
         'description': 'mit Hirtenkäse und Salat',
         'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
-        'price': 6.50,
+        'price': 6.50
     },
     {
         'meal': 'Döner Premium',
         'description': 'mit Drehspießfleisch und Sauce Hollandaise und mit Käse überbacken',
         'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Döner Spezial',
         'description': 'mit Drehspießfleisch, Pommes frites und Salat',
         'choice': 'mit Salatsauce, mit Tzatziki, ohne Sauce, ohne Eisbergsalat, ohne Gurken und mehr.',
-        'price': 8.50,
+        'price': 8.50
     }
 ]
 
 let teller = [
     {
+        'image': 'img/teller.jpg',
+        'title': 'Tellergerichte'
+    },
+    {
         'meal': 'Dönerteller',
         'description': 'mit Salat und Tzatziki',
         'choice': 'mit Dressing, italienisch, mit Salatsauce, ohne Dressing und mit Pizzabrot.',
-        'price': 11.50,
+        'price': 11.50
     },
     {
         'meal': 'Dönerteller Hollandaise',
         'description': 'mit Sauce Hollandaise, Salat und Tzatziki und mit Käse überbacken',
         'choice': '',
-        'price': 12.50,
+        'price': 12.50
     },
     {
         'meal': 'Dönerauflauf',
         'description': 'mit Nudeln und Sauce Hollandaise und mit Käse überbacken',
         'choice': 'mit Pommes frites, mit Reis oder ohne Beilage.',
-        'price': 10.50,
+        'price': 10.50
     },
     {
         'meal': 'Iskender',
         'description': 'Drehspießfleisch auf gerösteten Brotwürfeln mit spezieller Tomatensauce, dazu Salat und Salatsauce',
         'choice': 'mit Pommes frites, mit Reis oder ohne Beilage.',
-        'price': 12.00,
+        'price': 12.00
     },
     {
         'meal': 'Chicken Picks (12 Stück)',
         'description': 'mit Salat, ohne Drehspießfleisch',
         'choice': 'mit Pommes frites, mit Reis oder ohne Beilage.',
-        'price': 10.50,
+        'price': 10.50
     }
 ]
 
 let pasta = [
     {
+        'image': 'img/pasta.jpg',
+        'title': 'Pasta'
+    },
+    {
         'meal': 'Gnocchi mit Tomatensoße',
         'description': '',
         'choice': '',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Gnocchi Chef',
         'description': 'Kartoffelnudeln mit Sahnesauce, Champignons, Broccoli und mit Käse überbacken, ohne Brot',
         'choice': '',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Gnocchi Gorgonzola',
         'description': 'Kartoffelnudeln mit Sahnesauce, Champignons und Broccoli, ohne Brot',
         'choice': '',
-        'price': 10.00,
+        'price': 10.00
     },
     {
         'meal': 'Spaghetti Napoli',
         'description': 'mit Tomatensoße',
         'choice': '',
-        'price': 8.00,
+        'price': 8.00
     },
     {
         'meal': 'Spaghetti Bolognese',
         'description': 'mit Hackfleischsoße',
         'choice': '',
-        'price': 9.00,
+        'price': 9.00
     },
     {
         'meal': 'Spaghetti Carbonara',
         'description': 'mit Formvorderschinken und Ei-Sahnesauce',
         'choice': '',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Spaghetti Frutti di Mare',
         'description': 'mit Meeresfrüchten',
         'choice': '',
-        'price': 11.00,
+        'price': 11.00
     }
 ]
 
 let baguettes = [
     {
+        'image': 'img/baguette.jpg',
+        'title': 'Baguettes'
+    },
+    {
         'meal': 'Baguette Salami',
         'description': 'mit Tomaten und Zwiebeln',
         'choice': '',
-        'price': 9.00,
+        'price': 9.00
     },
     {
         'meal': 'Baguette Schinken',
         'description': 'mit Tomaten, Formvorderschinken und Zwiebeln',
         'choice': '',
-        'price': 9.00,
+        'price': 9.00
     },
     {
         'meal': 'Baguette Thunfisch',
         'description': 'mit Tomaten und Zwiebeln',
         'choice': '',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Baguette Hawaii',
         'description': 'mit Tomaten, Formvorderschinken und Ananas',
         'choice': '',
-        'price': 9.50,
+        'price': 9.50
     },
     {
         'meal': 'Baguette Mix',
         'description': '',
         'choice': '',
-        'price': 10.00,
+        'price': 10.00
     },
     {
         'meal': 'Baguette Döner',
         'description': 'mit Tomaten und Zwiebeln',
         'choice': '',
-        'price': 9.50,
+        'price': 9.50
     }
 ]
 
 let schnitzel = [
     {
+        'image': 'img/schnitzel.jpg',
+        'title': 'Schnitzel'
+    },
+    {
         'meal': 'Schnitzel Wiener Art',
         'description': '',
         'choice': '',
-        'price': 12.50,
+        'price': 12.50
     },
     {
         'meal': 'Schnitzel Gorgonzola',
         'description': 'mit Ananas und Sahnesauce',
         'choice': '',
-        'price': 13.50,
+        'price': 13.50
     },
     {
         'meal': 'Schnitzel Curry',
@@ -321,80 +357,79 @@ let schnitzel = [
         'meal': 'Schnitzel Käse',
         'description': 'mit vier Käsesorten und Sahnesauce',
         'choice': '',
-        'price': 14.00,
+        'price': 14.00
     },
     {
         'meal': 'Jägerschnitzel',
         'description': 'mit Jägersauce und Pilzen',
         'choice': '',
-        'price': 14.50,
+        'price': 14.50
     },
     {
         'meal': 'Zigeunerschitzel',
         'description': 'mit Zigeunersauce, Pilzen, Paprika, Zwiebeln und Peperoni',
         'choice': '',
-        'price': 15.00,
+        'price': 15.00
     },
     {
         'meal': 'Hawaii Schnitzel',
         'description': 'mit Ananas und Marsala-Sahnesauce und mit Käse überbacken',
         'choice': '',
-        'price': 15.00,
+        'price': 15.00
     }
 ]
 
 let burger = [
     {
+        'image': 'img/burger.jpg',
+        'title': 'Burger'
+    },
+    {
         'meal': 'Hamburger',
         'description': '',
         'choice': '',
-        'price': 6.00,
+        'price': 6.00
     },
     {
         'meal': 'Doppel Hamburger',
         'description': '',
         'choice': '',
-        'price': 7.50,
+        'price': 7.50
     },
     {
         'meal': 'Cheeseburger',
         'description': '',
         'choice': '',
-        'price': 6.50,
+        'price': 6.50
     },
     {
         'meal': 'Doppel Cheeseburger',
         'description': '',
         'choice': '',
-        'price': 8.50,
+        'price': 8.50
     },
     {
         'meal': 'Chickenburger',
         'description': '',
         'choice': '',
-        'price': 6.50,
+        'price': 6.50
     },
     {
         'meal': 'Dönerburger',
         'description': '',
         'choice': '',
-        'price': 6.50,
+        'price': 6.50
     }
 ]
 
 function render() {
     let dishes = [beliebt, salate, pizza, calzone, drehspieß, teller, pasta, baguettes, schnitzel, burger];
-    let dishesString = ['Beliebt', 'Salate', 'Pizza', 'Calzone', 'Drehspießgerichte', 'Tellergerichte', 'Pasta', 'Baguettes', 'Schnitzel', 'Burger'];
-
     for (let i = 0; i < dishes.length; i++) {
         const dish = dishes[i];
-
         let groups = document.getElementById('groups');
-        groups.innerHTML += /*html*/ `<h2 class="title">${dishesString[i]}</h2>`;
-
-        for (let j = 0; j < dish.length; j++) {
+        for (let j = 1; j < dish.length; j++) {
             const food = dish[j];
-
+            addHeadSection(groups, food, dish, i, j);
             groups.innerHTML += HTMLTemplate(food);
         }
     }
@@ -409,7 +444,29 @@ function HTMLTemplate(food) {
             </div>
             <p>${food['description']}</p>
             <p>${food['choice']}</p>
-            <h4>${food['price']}</h4>
+            <h4>${food['price']}€</h4>
         </div>
     `;
+}
+
+function addHeadSection(groups, food, dish, i, j) {
+    if (j == 1) {
+        j--;
+        food = dish[j];
+        groups.innerHTML += headSectionHTML(food, i, j);
+        control(food, i, j);
+    }
+}
+
+function headSectionHTML(food, i, j) {
+    return /*html*/ `
+        <img class="image d-none" id="image${i+j}" src="${food['image']}" alt="">
+        <h2>${food['title']}</h2>
+    `;
+}
+
+function control(food, i, j) {
+    if (food['image'] != '') {
+        document.getElementById(`image${i+j}`).classList.remove('d-none');
+    }
 }
